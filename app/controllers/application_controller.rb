92ace9d2933c866161388e89ebc82ec5ab25e621
Bismark <bismark64@gmail.com>
2014-04-15 19:59:21 -0300

    Fix account update permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:full_name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:account_update){|u| u.permit(:full_name, :email, :password, :password_confirmation, :current_password)}
   end
 
   def after_sign_in_path_for(resource)
