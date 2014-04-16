e20916ade0dbda7abcb30221aa58549419676ab8
Bismark <bismark64@gmail.com>
2014-04-16 12:46:30 -0300

    Improve cause selection Sponsor edit profile page

--- app/models/concerns/cause.rb
+++ app/models/concerns/cause.rb
@@ -2,35 +2,40 @@ module Cause
   extend ActiveSupport::Concern
 
   # DON'T CHANGE THE ORDER, IF YOU NEED TO ADD JUST APPEND TO THE LIST
-  CAUSES = [
-    "International Relief",
-    "US Relief",
-    "Education & Schools",
-    "Sports, Parks & Recreation",
-    "Medical, Hospitals & Clinics",
-    "Churches & Religion",
-    "Arts & Culture",
-    "Walks, Roads & Bridges",
-    "Infrastructure & Utility",
-    "Food & Hunger",
-    "Wildlife & Environment",
-    "Religious & Faith Based",
-    "Health & Medicine",
-    "Education & Learning",
-    "Economic Development",
-    "Freedom & Liberty",
-    "Alternative Media & Web",
-    "Creative Arts & Music",
-    "Community Events & Festivals",
-    "Politics & Campaigns",
-    "Individuals & Families in Need",
-    "Animals & Pets",
-    "Sports, Teams & Competitions",
-    "Business & Entrepeneurs",
-    "Group & Individual Projects",
-    "Military & Veterans",
-    "Memorials, Tributes & Awards"
-  ]
+  CAUSES_HIERARCHY = {
+    disaster_and_emergency_relief: ["International Relief", "US Relief"],
+    building_and_community_projects: [
+      "Education & Schools",
+      "Sports, Parks & Recreation",
+      "Medical, Hospitals & Clinics",
+      "Churches & Religion",
+      "Arts & Culture",
+      "Walks, Roads & Bridges",
+      "Infrastructure & Utility"
+    ],
+    causes_and_organizations: [
+      "Food & Hunger",
+      "Wildlife & Environment",
+      "Religious & Faith Based",
+      "Health & Medicine",
+      "Education & Learning",
+      "Economic Development",
+      "Freedom & Liberty",
+      "Alternative Media & Web",
+      "Creative Arts & Music",
+      "Community Events & Festivals",
+      "Politics & Campaigns",
+      "Individuals & Families in Need",
+      "Animals & Pets",
+      "Sports, Teams & Competitions",
+      "Business & Entrepeneurs",
+      "Group & Individual Projects",
+      "Military & Veterans",
+      "Memorials, Tributes & Awards"
+    ]
+  }
+
+  CAUSES = CAUSES_HIERARCHY.values.flatten
 
   def causes=(causes)
     self.causes_mask = (causes & CAUSES).map { |r| 2**CAUSES.index(r) }.inject(0, :+)
