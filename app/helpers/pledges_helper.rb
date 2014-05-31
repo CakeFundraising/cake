a2df17582555c7b347949e4c3a7ec91b55a21b41
Emiliano Coppo <bismark64@gmail.com>
2014-05-31 14:03:57 -0300

    Pledge wizard actions refactoring

--- app/helpers/pledges_helper.rb
+++ app/helpers/pledges_helper.rb
@@ -1,2 +1,9 @@
 module PledgesHelper
+  def cancel_pledge_link
+  	link_to 'Cancel', @pledge, data: {confirm: 'Are you sure?'}, method: :delete
+  end
+
+  def preview_pledge_link
+  	link_to "Preview", @pledge, class:'btn btn-primary pull-right'
+  end
 end
