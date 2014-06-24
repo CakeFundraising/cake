dd15c49647d047f84a2a28ab487b259dfe3012df
Emiliano Coppo <bismark64@gmail.com>
2014-06-24 17:17:28 -0300

    Views improvements

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -90,7 +90,7 @@ class Campaign < ActiveRecord::Base
     sponsor_categories.levels.each do |name, range|
       class_eval do
         define_method "#{name}_pledges" do
-          obj.pledges.accepted.total_amount_in(range).order('total_amount_cents DESC')
+          obj.pledges.accepted.total_amount_in(range).order(total_amount_cents: :desc, amount_per_click_cents: :desc)
         end 
       end
     end
