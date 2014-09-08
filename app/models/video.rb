18c8f067dcc8b14b3978c71965647b3def023360
Emiliano Coppo <bismark64@gmail.com>
2014-09-08 18:51:27 -0300

    Assets precompile

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,15 +1,18 @@
 class Video < ActiveRecord::Base
   belongs_to :recordable, polymorphic: true
-  before_save :extract_youtube_id
+  before_save :extract_video_id
 
-  REGEX = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
+  Y_REGEX = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
+  #V_REGEX = /^(?:https?:\/\/)?(?:www\.)?vimeo\.com\/(.+\/+)?(\d+)/
 
   validates :url, :recordable_type, :recordable_id, presence: true
-  validates :url, format: { with: REGEX, message: "Invalid youtube link." }, if: :new_record?
+  validates :url, format: { with: Y_REGEX, message: "Invalid Youtube link." }, if: :new_record?
+  #validates :url, format: { with: V_REGEX, message: "Invalid Vimeo link." }, if: :new_record?
 
   private
 
-  def extract_youtube_id
-    self.url = self.url.match(REGEX)[5] unless self.url.match(REGEX).nil?
+  def extract_video_id
+    self.url = self.url.match(Y_REGEX)[5] unless self.url.match(Y_REGEX).nil?
+    #self.url = self.url.match(V_REGEX)[2] unless self.url.match(Y_REGEX).nil?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908160542) do
+ActiveRecord::Schema.define(version: 20140908211741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -380,6 +380,7 @@ ActiveRecord::Schema.define(version: 20140908160542) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
   end
 
 end
