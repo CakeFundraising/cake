a289bafd505c9cd10440e9d87c8c11b064a05785
Emiliano Coppo <bismark64@gmail.com>
2014-09-15 12:34:47 -0300

    Add vimeo to videos

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -2,10 +2,26 @@ class VideoDecorator < ApplicationDecorator
   delegate_all
 
   def thumbnail
-    image_tag "http://img.youtube.com/vi/#{object.url}/1.jpg", :class => :thumbnail if object.url.present?
+    h.image_tag object.thumbnail, class: :thumbnail if object.thumbnail.present?
   end
 
   def iframe
-    %Q{<iframe id="youtube_video" title="YouTube video player" width="100%" height="400px" src="http://www.youtube.com/embed/#{object.url}" frameborder="0" allowfullscreen></iframe>}.html_safe if object.url.present?
+    if object.url.present?
+      if provider == 'youtube'
+        %Q{<iframe id="youtube_video" title="YouTube video player" width="100%" height="400px" src="http://www.youtube.com/embed/#{object.url}" frameborder="0" allowfullscreen></iframe>}.html_safe
+      elsif provider == 'vimeo'
+        %Q{<iframe src="//player.vimeo.com/video/#{object.url}" width="100%" height="400px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>}.html_safe
+      end
+    end
+  end
+
+  def input_value
+    if object.url.present?
+      if provider == 'youtube'
+        "https://www.youtube.com/watch?v=#{object.url}"
+      elsif provider == 'vimeo'
+        "https://www.vimeo.com/#{object.url}"
+      end
+    end
   end
 end
