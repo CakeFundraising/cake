e1a8cb2b240ce005a914b5f2e51dd048d68c1e22
Emiliano Coppo <bismark64@gmail.com>
2014-09-18 19:38:30 -0300

    Remove Carrierwave avatar mounter

--- app/models/sweepstake.rb
+++ app/models/sweepstake.rb
@@ -4,10 +4,6 @@ class Sweepstake < ActiveRecord::Base
   belongs_to :pledge
   has_one :sponsor, through: :pledge
 
-  mount_uploader :avatar, AvatarUploader
-  validates_integrity_of  :avatar
-  validates_processing_of :avatar
-
   validates :title, :avatar, :winners_quantity, :description, :terms_conditions, :claim_prize_instructions, :pledge, presence: true
 
   after_initialize do
