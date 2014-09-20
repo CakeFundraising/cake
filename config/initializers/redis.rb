c89915e0a747eb3249c56924cf75ec0a29eca1ab
Emiliano Coppo <bismark64@gmail.com>
2014-09-19 21:52:36 -0300

    Assets precompile & Fix Redis/precompile issue

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -8,7 +8,7 @@ unless Rails.env.test?
       user == 'cake'
       password == 'password'
     end
-  elsif Rails.env.production?
+  elsif Rails.env.production? and ENV["REDISTOGO_URL"].present?
     uri = URI.parse(ENV["REDISTOGO_URL"])
     Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
