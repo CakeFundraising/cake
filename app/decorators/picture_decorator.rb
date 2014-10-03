1f8bc039734d8525b6b211d08834b7f34fc4cbc2
Emiliano Coppo <bismark64@gmail.com>
2014-10-03 13:17:17 -0300

    Add avatar_path to PicturesDecorator

--- app/decorators/picture_decorator.rb
+++ app/decorators/picture_decorator.rb
@@ -1,12 +1,12 @@
 class PictureDecorator < ApplicationDecorator
   delegate_all
 
+  #Image tags
   def avatar(options={})
     if object.avatar.present?
       options = {crop: :crop, width: object.avatar_crop_w, height: object.avatar_crop_h, x: object.avatar_crop_x, y: object.avatar_crop_y, class: 'img-thumbnail img-responsive img-loaded'}.merge options
       h.cl_image_tag(object.avatar, options)
     else
-      #h.image_tag 'http://www.placehold.it/305x230&text=no+image+available', class: 'img-thumbnail img-responsive'
       h.image_tag 'placeholder_avatar.png', class: 'img-thumbnail img-responsive img-default-avatar'
     end
   end
@@ -16,18 +16,7 @@ class PictureDecorator < ApplicationDecorator
       options = {crop: :crop, width: object.banner_crop_w, height: object.banner_crop_h, x: object.banner_crop_x, y: object.banner_crop_y, class: 'img-thumbnail img-responsive img-loaded'}.merge options
       h.cl_image_tag(object.banner, options)      
     else
-      #h.image_tag 'http://www.placehold.it/342x150&text=no+image+available', class: 'img-thumbnail img-responsive'
       h.image_tag 'placeholder_banner.png', class: 'img-thumbnail img-responsive img-default-banner'
-    end
-  end
-
-  def banner_path(options={})
-    if object.banner.present?
-      options = {crop: :crop, width: object.banner_crop_w, height: object.banner_crop_h, x: object.banner_crop_x, y: object.banner_crop_y}.merge options
-      h.cl_image_path(object.banner, options)    
-    else
-      #h.image_tag 'http://www.placehold.it/1400x614&text=no+image+available'
-      h.image_tag 'placeholder_banner.png', class: 'img-thumbnail img-responsive'
     end
   end
 
@@ -36,8 +25,27 @@ class PictureDecorator < ApplicationDecorator
       options = {crop: :crop, width: object.qrcode_crop_w, height: object.qrcode_crop_h, x: object.qrcode_crop_x, y: object.qrcode_crop_y, class: 'img-thumbnail img-responsive img-loaded'}.merge options
       h.cl_image_tag(object.qrcode, options)
     else
-      #h.image_tag 'http://www.placehold.it/300x300&text=no+image+available', class: 'img-thumbnail img-responsive'
       h.image_tag 'placeholder.png', class: 'img-thumbnail img-responsive'
     end
   end
+
+  #Image paths
+  def avatar_path(options={})
+    if object.avatar.present?
+      options = {crop: :crop, width: object.avatar_crop_w, height: object.avatar_crop_h, x: object.avatar_crop_x, y: object.avatar_crop_y}.merge options
+      h.cl_image_path(object.avatar, options)    
+    else
+      h.image_path 'placeholder_avatar.png', class: 'img-thumbnail img-responsive'
+    end
+  end
+
+  def banner_path(options={})
+    if object.banner.present?
+      options = {crop: :crop, width: object.banner_crop_w, height: object.banner_crop_h, x: object.banner_crop_x, y: object.banner_crop_y}.merge options
+      h.cl_image_path(object.banner, options)    
+    else
+      h.image_path 'placeholder_banner.png', class: 'img-thumbnail img-responsive'
+    end
+  end
+
 end
