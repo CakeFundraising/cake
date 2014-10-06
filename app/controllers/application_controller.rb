e40199db57eef712166897bce5669826c8b5499a
jacobbeckley <jacobbeckley@yahoo.com>
2014-10-06 18:12:33 -0500

    New FB Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  skip_before_filter :verify_authenticity_token
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  #before_filter :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_fundraiser, :current_sponsor
 
   rescue_from CanCan::AccessDenied do |exception|
