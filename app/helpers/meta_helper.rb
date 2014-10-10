23ce91cc0129ea400ab232a510a72120db31df84
jacobbeckley <jacobbeckley@yahoo.com>
2014-10-10 12:51:33 -0500

    Replace MetaMagic with Meta-Tags

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -1,16 +1,19 @@
 module MetaHelper
 	def display_meta(title, og=nil, description=nil, keywords=nil)
-		description = t('application.meta.description') if description.nil?
-		
-		og = {
-			title: t('application.meta.og.title'), 
-			image: image_url("fb_share.png"), 
-			url: request.original_url, 
-			description: t('application.meta.description') 
-		} if og.nil?
-
 		content_tag(:span, class: 'meta') do
-			meta title: title, description: description, keywords: keywords, og: og
+			set_meta_tags :description => t('application.meta.description') if description.nil?
+			set_meta_tags :keywords => t('application.meta.keywords') if keywords.nil?
+			set_meta_tags :og => {
+				title: t('application.meta.og.title'), 
+				image: image_url("fb_share.png"), 
+				url: request.original_url, 
+				description: t('application.meta.description') 
+			} if og.nil?
+
+			set_meta_tags :title => title if !title.nil?
+			set_meta_tags :description => description if !description.nil?
+			set_meta_tags :keywords => keywords if !keywords.nil?
+			set_meta_tags :og => og if !og.nil?
 		end
 	end
 end
