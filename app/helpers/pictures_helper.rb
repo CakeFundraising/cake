560726bcc839158b90b9e74de731d6a1b7196db1
jacobbeckley <jacobbeckley@yahoo.com>
2014-10-16 11:10:38 -0500

    CSS updates

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -5,8 +5,13 @@ module PicturesHelper
   end
 
   def screenshot_tag_for(object, options={})
-    options = {type: 'url2png', sign_url: true, class: 'img-responsive'}.merge options
-    cl_image_tag(object, options)
+    #options = {type: 'url2png', :type => "url2png", :transformation => [{ :width => 400, :height => 400, :gravity => "north", :radius => 50, :border => "2px_solid_rgb:999"}], :sign_url => true, class: 'img-responsive'}.merge options
+    cl_image_tag(object,
+      :type => "url2png", 
+      :transformation => [ 
+        { :crop => "fill", :width => 500, :gravity => "north"}], 
+      :sign_url => true, :class => 'img-responsive')
+    #cl_image_tag(object, options)
     #cl_image_tag(url_for(:controller => object.object.class.name.downcase.pluralize, :action => 'show', :id => object.id, :only_path => false), options)
   end
 
