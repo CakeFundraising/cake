e46187a303cb314088e4653c08295ef234bcc678
jacobbeckley <jacobbeckley@yahoo.com>
2014-11-24 09:51:19 -0600

    New Updated Screenshot Creation

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -5,40 +5,8 @@ module PicturesHelper
   end
 
   def screenshot_tag_for(object, options={})
-    #cl_image_tag(object, 
-    #  :type => "url2png",
-    #  :transformation => [
-    #    { :crop => "fill", :width => 500, :gravity => "north"}], 
-    #  :sign_url => true, :class => 'img-responsive')
-
-    #options = {type: 'url2png', :type => "url2png", :transformation => [{ :width => 400, :height => 400, :gravity => "north", :radius => 50, :border => "2px_solid_rgb:999"}], :sign_url => true, class: 'img-responsive'}.merge options
-    
-
-    #Cloudinary::Uploader.explicit(object, :type => "url2png")
-
-    #Cloudinary::Uploader.explicit(object,
-    #  :type => "url2png", :force => true,
-    #  :transformation => [ 
-    #    { :crop => "fill", :width => 500, :gravity => "north"}], 
-    #  :sign_url => true, :class => 'img-responsive')
-
-
-    #cloudinary_url(object,
-    #  :type => "url2png", 
-    #  :crop => "fill", :width => 500, :gravity => :north, 
-    #  :sign_url => true, :class => 'img-responsive')
-
-    #Cloudinary::Uploader.explicit
-    #cl_image_tag(screenshot)
-
-    #cloudinary_url(object,
-    #  :type => "url2png", 
-    #  :transformation => [ 
-    #    { :crop => "fill", :width => 500, :gravity => "north"}], 
-    #  :sign_url => true, :class => 'img-responsive')
-
-    #cl_image_tag(object, options)
-    #cl_image_tag(url_for(:controller => object.object.class.name.downcase.pluralize, :action => 'show', :id => object.id, :only_path => false), options)
+    new_screenshot = Cloudinary::Uploader.explicit(object, :type => "url2png")
+    cl_image_tag(object, :type => "url2png", :version => new_screenshot["version"], :crop => "fill", :width => 500, :gravity => "north", :sign_url => true, :class => 'img-responsive')
   end
 
   def banner_tag_for(object, options={})
