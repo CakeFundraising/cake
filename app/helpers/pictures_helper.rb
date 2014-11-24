f37b86ce6d9b625f8b8a2fd3a641f0fdcaff00e9
jacobbeckley <jacobbeckley@yahoo.com>
2014-11-24 10:18:10 -0600

    Attempt

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -5,9 +5,11 @@ module PicturesHelper
   end
 
   def screenshot_tag_for(object, options={})
-    Cloudinary::Uploader.explicit(object, :type => "url2png")
+    #http://res.cloudinary.com/cakefundraising/image/url2png/http://staging.cakefundraising.com/campaigns/187
+    #http://res.cloudinary.com/cakefundraising/image/url2png/s--iV322qFH--/c_fill,g_north,w_500/v1416845685/http://staging.cakefundraising.com/campaigns/187
     new_screenshot = Cloudinary::Uploader.explicit(object, :type => "url2png")
-    cl_image_tag(object, :type => "url2png", :version => new_screenshot["version"], :crop => "fill", :width => 500, :gravity => "north", :sign_url => true, :class => 'img-responsive')
+    cl_image_tag(object, :type => "url2png", :class => 'img-responsive')
+    #cl_image_tag(object, :type => "url2png", :version => new_screenshot["version"], :crop => "fill", :width => 500, :gravity => "north", :sign_url => true, :class => 'img-responsive')
   end
 
   def banner_tag_for(object, options={})
