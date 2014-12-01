49f16f63a879f97cc459eb60ecae5661ada5d9f8
Emiliano Coppo <bismark64@gmail.com>
2014-12-01 17:21:59 -0300

    Update both screenshot and page url

--- app/models/concerns/screenshotable.rb
+++ app/models/concerns/screenshotable.rb
@@ -5,6 +5,7 @@ module Screenshotable
   def update_screenshot(url)
     screenshot_url = Cloudinary::Uploader.explicit(url, :type => "url2png")["url"].gsub('http:', 'https:')
     self.update_attribute(:screenshot_url, screenshot_url)
+    FacebookOpenGraph.clear_cache(screenshot_url)
     FacebookOpenGraph.clear_cache(url)
   end
 end
