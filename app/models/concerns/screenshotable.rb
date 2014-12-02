115fe0bf2c38583a8c84959b078e54ce817db764
jacobbeckley <jacobbeckley@yahoo.com>
2014-12-02 10:25:09 -0600

    Updated Code

--- app/models/concerns/screenshotable.rb
+++ app/models/concerns/screenshotable.rb
@@ -3,8 +3,9 @@ module Screenshotable
   extend ActiveSupport::Concern
 
   def update_screenshot(url)
-    screenshot_url = Cloudinary::Uploader.explicit(url, :type => "url2png")["url"]
-    screenshot_version = Cloudinary::Uploader.explicit(url, :type => "url2png")["version"]
+    cloudinary_obj = Cloudinary::Uploader.explicit(url, :type => "url2png")
+    screenshot_url = cloudinary_obj["url"]
+    screenshot_version = cloudinary_obj["version"]
     self.update_attribute(:screenshot_url, screenshot_url)
     self.update_attribute(:screenshot_version, screenshot_version)
     FacebookOpenGraph.clear_cache(url)
