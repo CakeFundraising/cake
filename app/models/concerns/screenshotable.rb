01a1d9e3115734888f2e741bf5add2993e0690e1
Emiliano Coppo <bismark64@gmail.com>
2014-12-02 16:00:33 -0300

    Use string interpolation on screenshot version

--- app/models/concerns/screenshotable.rb
+++ app/models/concerns/screenshotable.rb
@@ -5,7 +5,7 @@ module Screenshotable
   def update_screenshot(url)
     cloudinary_obj = Cloudinary::Uploader.explicit(url, :type => "url2png")
     screenshot_url = cloudinary_obj["url"].gsub('http:', 'https:')
-    screenshot_version = "v" + cloudinary_obj["version"]
+    screenshot_version = "v#{cloudinary_obj['version']}"
     self.update_attribute(:screenshot_url, screenshot_url)
     self.update_attribute(:screenshot_version, screenshot_version)
     FacebookOpenGraph.clear_cache(url)
