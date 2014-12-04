f5f748766f3f2c7f30e637b17e67c96dde4c37a3
Emiliano Coppo <bismark64@gmail.com>
2014-12-04 12:34:17 -0300

    Add scopes to Browser/Click

--- app/models/browser.rb
+++ app/models/browser.rb
@@ -1,10 +1,13 @@
 class Browser < ActiveRecord::Base
   belongs_to :user
 
-  validates :token, :fingerprint, presence: true
+  has_many :clicks, -> { where(bonus: false) }, class_name: 'Click', dependent: :destroy
+  has_many :bonus_clicks, -> { where(bonus: true) }, class_name: 'Click', dependent: :destroy
 
   scope :with_fingerprint, ->(fingerprint){ where(fingerprint: fingerprint) }
   scope :with_token, ->(token){ where(token: token) }
 
   scope :equal_to, ->(other){ where(fingerprint: other.fingerprint, token: other.token) }
+
+  validates :token, :fingerprint, presence: true
 end
--- app/models/click.rb
+++ app/models/click.rb
@@ -9,4 +9,8 @@ class Click < ActiveRecord::Base
   }
 
   validate :browser_id, :pledge_id, presence: true
+
+  scope :with_browser, ->{ eager_load(:browser) }
+  scope :token, ->(token){ where('browsers.token = ?', token) }
+  scope :fingerprint, ->(fingerprint){ where('browsers.fingerprint = ?', fingerprint) }
 end
