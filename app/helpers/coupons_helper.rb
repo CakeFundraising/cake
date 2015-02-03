34aa0ec263555313f861265d94295cb4caf4052e
Emiliano Coppo <bismark64@gmail.com>
2015-02-03 16:04:08 -0300

    Fix coupon actions cache issue

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -5,7 +5,7 @@ module CouponsHelper
     else
       # Link with rollover
       picture_rollover(coupon.picture.avatar, coupon.url) do
-        content_tag(:div, 'Learn more') + content_tag(:div, 'about this offer!')
+        content_tag(:div, 'Click to learn more') + content_tag(:div, 'about this offer!')
       end
     end
   end
