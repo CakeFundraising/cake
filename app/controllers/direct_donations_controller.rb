65aba4640971b88a4e9cad18aeb3f89476f7574d
andrewoconnor <andrewoconnor@outlook.com>
2015-02-20 16:00:18 -0500

    direct donation to fundraiser

--- app/controllers/direct_donations_controller.rb
+++ app/controllers/direct_donations_controller.rb
@@ -1,14 +1,27 @@
 class DirectDonationsController < InheritedResources::Base
   def create
-    @campaign = Campaign.find(permitted_params[:direct_donation][:campaign_id])
-    @direct_donation = @campaign.direct_donations.build(permitted_params[:direct_donation])    
+    @fundraiser = Fundraiser.find(permitted_params[:direct_donation][:fundraiser_id])
+    @direct_donation = @fundraiser.direct_donations.build(permitted_params[:direct_donation])
+
+    @campaign
+    if permitted_params[:direct_donation][:campaign_id].present?
+      @campaign = Campaign.find(permitted_params[:direct_donation][:campaign_id])
+    end
 
     create! do |success, failure|
       success.html do
-        redirect_to @campaign, notice: "Thanks for contributing!"
+        if @campaign
+          redirect_to @campaign, notice: "Thanks for contributing!"
+        else
+          redirect_to @fundraiser, notice: "Thanks for contributing!"
+        end
       end
       failure.html do
-        redirect_to @campaign, alert: "There was an error with your donation, please try again.."
+        if @campaign
+          redirect_to @campaign, alert: "There was an error with your donation, please try again."
+        else
+          redirect_to @fundraiser, alert: "There was an error with your donation, please try again."
+        end
       end
     end
   end
@@ -16,6 +29,6 @@ class DirectDonationsController < InheritedResources::Base
   protected
 
   def permitted_params
-    params.permit(direct_donation: [:campaign_id, :card_token, :amount, :email])
+    params.permit(direct_donation: [:fundraiser_id, :campaign_id, :card_token, :amount, :email])
   end
 end
