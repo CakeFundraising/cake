c30506de036b79c858850eae8571a13c2464d940
Emiliano Coppo <bismark64@gmail.com>
2015-02-24 13:06:50 -0300

    Remove duplicate association in QP factory

--- spec/factories/quick_pledges.rb
+++ spec/factories/quick_pledges.rb
@@ -3,7 +3,6 @@
 FactoryGirl.define do
   factory :quick_pledge, class: QuickPledge, parent: :pledge do
     type 'QuickPledge'
-    campaign
     association :campaign, factory: :regular_campaign
   end
 end
