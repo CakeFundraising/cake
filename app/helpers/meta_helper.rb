1f0a3100c770e8f9234a97aef926bc34656fe2a8
Emiliano Coppo <bismark64@gmail.com>
2015-04-18 17:43:42 -0300

    Meta Title fix

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -1,19 +1,7 @@
 module MetaHelper
-	def display_meta(title, og=nil, description=nil, keywords=nil)
+	def display_meta(title)
 		content_tag(:span, class: 'meta') do
-			set_meta_tags :description => t('application.meta.description') if description.nil?
-			set_meta_tags :keywords => t('application.meta.keywords') if keywords.nil?
-			set_meta_tags :og => {
-				title: t('application.meta.og.title'), 
-				image: image_url("fb_share.png"), 
-				url: request.original_url, 
-				description: t('application.meta.description') 
-			} if og.nil?
-
-			set_meta_tags :title => title if !title.nil?
-			set_meta_tags :description => description if !description.nil?
-			set_meta_tags :keywords => keywords if !keywords.nil?
-			set_meta_tags :og => og if !og.nil?
+			meta {title: title}
 		end
 	end
 
