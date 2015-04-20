6169d9076a2ccbd684b00fed20053c46e2bdfbc5
Emiliano Coppo <bismark64@gmail.com>
2015-04-20 12:47:52 -0300

    HeroDecorator missing methods fix

--- app/decorators/hero_decorator.rb
+++ app/decorators/hero_decorator.rb
@@ -1,17 +1,7 @@
 class HeroDecorator < CampaignDecorator
+  decorates :campaign
+
   def instance_name
     "hero"
-  end
-
-  def class_name
-    "Campaign"  
-  end
-
-  def self.primary_key
-    'id'
-  end
-
-  def self.base_class
-    Campaign
   end
 end
